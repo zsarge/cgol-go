feccd679908da79f4640a9676de8aa245eeb3caa
zsarge <zackarywsargent@gmail.com>
2021-08-05 14:37:06 -0400

    represent a square

--- src/main.go
+++ src/main.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Hello World!")
+type square bool
+
+// a square can be alive or dead
+const (
+	Alive square = true
+	Dead square = false
+)
+
+func (s square) display() {
+	if s == Alive {
+		fmt.Println("Square is Alive!")
+	} else {
+		fmt.Println("Square is Dead!")
+	}
 }
+
+func main() {
+	d := Dead
+	d.display()
+}
+
