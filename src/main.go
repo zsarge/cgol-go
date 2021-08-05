385a6907492e576083d205f8fa4cd0f62fb32ed7
zsarge <zackarywsargent@gmail.com>
2021-08-05 14:54:18 -0400

    create grid of dead squares

--- src/main.go
+++ src/main.go
@@ -14,14 +14,52 @@ const (
 
 func (s square) display() {
 	if s == Alive {
-		fmt.Println("Square is Alive!")
+		fmt.Print("X")
 	} else {
-		fmt.Println("Square is Dead!")
+		fmt.Print(" ")
 	}
 }
 
+type board struct {
+	height int
+	width int
+	squares [][]square
+}
+
+func (b *board) init(height, width int) {
+	b.height = height
+	b.width = width
+
+	b.squares = make([][]square, height)
+	for i := 0; i < height; i++ {
+		    b.squares[i] = make([]square, width)
+	}
+}
+
+func (b board) printHeader() {
+	fmt.Print("+")
+	for x := 0; x < b.width; x++ {
+		fmt.Print("-")
+	}
+	fmt.Println("+")
+}
+
+func (b board) display() {
+	b.printHeader()
+	for y := 0; y < b.height; y++ {
+		fmt.Print("|")
+		for x := 0; x < b.width; x++ {
+			b.squares[y][x].display()
+		}
+		fmt.Println("|")
+	}
+	b.printHeader()
+}
+
+
 func main() {
-	d := Dead
-	d.display()
+	b := new(board)
+	b.init(10,10)
+	b.display()
 }
 
